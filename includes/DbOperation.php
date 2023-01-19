8507a313c2efaf2596babc00eebe0a2380a2dfc9
prak899 <majhi9442@gmail.com>
2023-01-19 11:40:43 +0530

    2 more api call added!

--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -21,19 +21,61 @@ class DbOperation
     
     //fetching all records from the database 
     public function getArtists(){
-        $stmt = $this->con->prepare("SELECT id, first_name, last_name FROM DRM");
-        $stmt->execute();
-        $stmt->bind_result($id, $first_name, $last_name);
+        $stmt = $this -> con -> prepare("SELECT id, first_name, last_name FROM DRM");
+        $stmt -> execute();
+        $stmt -> bind_result($id, $first_name, $last_name);
         $artists = array();
         
-        while($stmt->fetch()){
-        $temp = array(); 
-        $temp['ID'] = $id; 
-        $temp['First Name'] = $first_name; 
-        $temp['Last Name'] = $last_name; 
-        array_push($artists, $temp);
+        while($stmt -> fetch()){
+            $temp = array(); 
+            $temp['ID'] = $id; 
+            $temp['First Name'] = $first_name; 
+            $temp['Last Name'] = $last_name; 
+            array_push($artists, $temp);
         }
         return $artists; 
+    }
+
+    //login user using credentials
+    public function loginUser(){
+        $stmt = $this -> con -> prepare("SELECT * FROM DRM");
+        $stmt -> execute();
+        $stmt -> bind_result($id, $first_name, $last_name, $employee_code, $phone_number);
+        $users = array();
+
+        while($stmt -> fetch()){
+            $temp = array();
+            $temp['ID'] = $id;
+            $temp['First Name'] = $first_name;
+            $temp['Last Name'] = $last_name;
+            $temp['Employee Code'] = $employee_code;
+            $temp['Phone Number'] = $phone_number;
+            array_push($users, $temp);
+        }
+
+        return $users;
+    }
+
+    //search user by primary key
+    public function searchUsers($id){
+        $stmt = $this->con->prepare("SELECT * FROM DRM WHERE id = (?)");
+        $stmt->bind_param("s", $id);
+        $stmt -> bind_result($id, $first_name, $last_name, $employee_code, $phone_number);
+        $stmt->execute();
+
+        $users = array();
+
+        while($stmt -> fetch()){
+            $temp = array();
+            $temp['ID'] = $id;
+            $temp['First Name'] = $first_name;
+            $temp['Last Name'] = $last_name;
+            $temp['Employee Code'] = $employee_code;
+            $temp['Phone Number'] = $phone_number;
+            array_push($users, $temp);
+        }
+
+        return $users;
     }
 }
 
--- v1/index.php
+++ v1/index.php
@@ -12,49 +12,81 @@
  //switching the get op value 
  switch($_GET['op']){
 
- case 'drmdata':
- if(isset ($_POST['first_name']) and isset($_POST['last_name']) and isset($_POST['employee_code']) and isset($_POST['phone_number'])){
- $db = new DbOperation(); 
+//add drm to database
+case 'adddrm':
+    if(isset ($_POST['first_name']) and isset($_POST['last_name']) and isset($_POST['employee_code']) and isset($_POST['phone_number'])){
+    $db = new DbOperation(); 
 
- if($db->createArtist($_POST['first_name'], $_POST['last_name'], $_POST['employee_code'], $_POST['phone_number'])){
-        $response['error'] = false;
-        $response['message'] = 'Sucess!';
- }else{
-        $response['error'] = true;
-        $response['message'] = 'Could not add data';
- }
- }else{
-        $response['error'] = true; 
-        $response['message'] = 'Required Parameters are missing';
- }
- break; 
+    if($db->createArtist($_POST['first_name'], $_POST['last_name'], $_POST['employee_code'], $_POST['phone_number'])){
+            $response['error'] = false;
+            $response['message'] = 'Sucess!';
+    }else{
+            $response['error'] = true;
+            $response['message'] = 'Could not add data';
+    }
+    }else{
+            $response['error'] = true; 
+            $response['message'] = 'Required Parameters are missing';
+    }
+    break;
+   
+//serach users by primary key    
+case 'searchdrm':
+    if(isset ($_POST['id'])){
+    $db = new DbOperation(); 
+    $artists = $db -> searchUsers($_POST['id']);
+
+
+    if($db->searchUsers($_POST['id'])){
+            $response['error'] = false;
+            $response['message'] = $artists;
+    }else{
+            $response['error'] = true;
+            $response['message'] = 'Could not add data';
+    }
+    }else{
+            $response['error'] = true; 
+            $response['message'] = 'Required Parameters are missing';
+    }
+    break; 
  
- //if it is getartist that means we are fetching the records
- case 'drmlist':
- $db = new DbOperation();
- $artists = $db->getArtists();
- if(count($artists)<=0){
-    
+//if it is getartist that means we are fetching the records
+case 'drmlist':
+    $db = new DbOperation();
+    $artists = $db -> getArtists();
+    if(count($artists) <= 0){
+            $response['error'] = true;
+            $response['message'] = 'Nothing found in the database';
+    }else{     
+            $response['error'] = false; 
+            $response['artists'] = $artists;
+    }
+    break; 
+
+//case for all users
+case 'users':
+    $db = new DbOperation();
+    $users = $db -> loginUser();
+    if(count($users) <= 0){
         $response['error'] = true;
-        $response['message'] = 'Nothing found in the database';
- }else{
-        
+        $response['message'] = 'No data!';
+    }
+    else{
         $response['error'] = false; 
-        $response['artists'] = $artists;
- }
- break; 
- 
- default:
+        $response['artists'] = $users;
+    }
+    break;
+
+default:
         $response['error'] = true;
         $response['message'] = 'No operation to perform';
- 
- }
+}
  
  }else{
         $response['error'] = false; 
         $response['message'] = 'Invalid Request';
  }
  
- //displaying the data in json 
+    //displaying the data in json 
     echo json_encode($response);
  ?>
